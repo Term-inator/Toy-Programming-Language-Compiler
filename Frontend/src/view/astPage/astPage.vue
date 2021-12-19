da2fd3a7697138331321b50b474740874ce7a834
Term-inator <1121745627@qq.com>
2021-12-19 16:08:33 +0800

    ast

--- Frontend/src/view/astPage/astPage.vue
+++ Frontend/src/view/astPage/astPage.vue
@@ -1,7 +1,12 @@
 <template>
     <div class='astpage'>
         <div class='main'>
-            
+            <Row>
+                <Button size='large' @click='reloadTree'>重新加载</Button>
+            </Row>
+            <Row>
+                <svg></svg>
+            </Row>
         </div>
     </div>
 </template>
@@ -16,37 +21,50 @@ export default {
     },
     data() {
         return {
-            svg: null,
-            simple_ast: null
+            svg_width: '90vw',
+            svg_height: '80vh',
+            simple_ast: null,
+            tree_height: 0,
+            tree_width: 0
         }
     },
     mounted() {
-        this.svg = d3.select('.main')
-                    .append('svg')
-                    .attr('width', '100%')
-                    .attr('height', '100%')
-        console.log(this.svg)
-        this.getSimpleAst()
-        this.generateAstTree()
+        d3.select('svg')
+            .attr('width', this.svg_width)
+            .attr('height', this.svg_height)
+            .style('border', '2px solid rgba(0, 0, 0, .3)')
+            .style('border-radius', '10px')
+        if(this.$store.state.syntax_ast === null) {
+            console.log("ast is null")
+        }
+        else {
+            this.getSimpleAst()
+            this.generateAstTree()
+        }
     },
     methods: {
+        reloadTree() {
+            d3.select('g').remove()
+            this.tree_width = 0
+            this.tree_height = 0
+            this.getSimpleAst()
+            this.generateAstTree()
+        },
         getSimpleAst() {
-            if(this.$store.state.syntax_ast === null) {
-                console.log("ast is null")
-                return
-            }
             this.simple_ast = this.generateSimpleAst(this.$store.state.syntax_ast)
             console.log(this.simple_ast)
         },
         generateSimpleAst(root) {
             let node = {val: 0, children: null}
             if(root.children !== null) {
+                ++this.tree_height
                 let new_children = []
                 root.children.forEach((node) => {
                     new_children.push(this.generateSimpleAst(node))
                 })
                 node.children = new_children
             }
+            ++this.tree_width
             if(root.type === 0) {
                 node.val = root.val.attr_val
             }
@@ -56,49 +74,55 @@ export default {
             return node
         },
         generateAstTree() {
+            let tree_group = d3.select('svg')
+                                .append('g')
+
+            d3.select('svg')
+                .call(d3.behavior.zoom().on('zoom', () => {
+                    tree_group.attr("transform", "translate("  +d3.event.translate + ")scale(" +d3.event.scale + ")")
+                }))
+
             let tree = d3.layout.tree()
-                        .size([2000, 1500])
+                        .size([this.tree_width * 30, this.tree_height * 30])
                         .separation((a, b) => {
                             return (a.parent == b.parent ? 1 : 2)
                         })
 
             let diagonal = d3.svg.diagonal()
-                            .projection(function (d) {
+                            .projection((d) => {
                                 return [d.x, d.y];
                             })
 
             let nodes = tree.nodes(this.simple_ast)
             let links = tree.links(nodes)
 
-            let link = d3.select('svg').selectAll(".link")
+            let link = tree_group.selectAll(".link")
                 .data(links)
                 .enter()
                 .append("path")
-                .attr("class", "link")
                 .attr("d", diagonal)
+                .style('fill', 'none')
+                .style('stroke', 'steelblue')
+                .style('stroke-width', '1.5px')
 
-            let node = d3.select('svg').selectAll(".node")
+            let node = tree_group.selectAll(".node")
                 .data(nodes)
                 .enter()
                 .append("g")
-                .attr("class", "node")
-                .attr("transform", function (d) {
+                .attr("transform", (d) => {
                     return "translate(" + d.x + "," + d.y + ")";
                 })
 
-            // node.append("circle")
-            //     .attr("r", 10)
-            //     .style('fill', 'none')
+            node.append("circle")
+                .attr("r", 20)
+                .style('fill', 'white')
+                .style('stroke', 'steelblue')
+                .style('stroke-width', '1.5px')
 
             node.append("text")
-                // .attr("dx", function (d) {
-                //     return d.children ? -8 : 8;
-                // })
                 .attr("dy", 3)
-                // .style("text-anchor", function (d) {
-                //     return d.children ? "end" : "start";
-                // })
-                .text(function (d) {
+                .style("text-anchor", 'middle')
+                .text((d) => {
                     return d.val;
                 });
         }
@@ -117,17 +141,7 @@ export default {
 
 .astpage .main {
     height: inherit;
-    padding: 5vh 5vw 0 5vw;
-}
-
-.node circle {
-    fill: #fff;
-    stroke: steelblue;
-    stroke-width: 1.5px;
-}
-
-.node {
-    font: 12px sans-serif;
+    padding: 4vh 5vw 0 5vw;
 }
 
 .link {
